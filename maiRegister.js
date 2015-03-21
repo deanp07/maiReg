75db25d8746698b14663f33bdbaf9c859181500d
Dean P <infiknotwebdev@gmail.com>
2015-03-21 20:31:44 +0200

    building templates

--- maiRegister.js
+++ maiRegister.js
@@ -1,26 +1,2 @@
 //register DB
-MyReg = new Mongo.Collection("myReg");
-
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Products = new Mongo.Collection("products");
